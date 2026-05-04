aa23ef368a1d93acc5e3f79b84dc83bd16dc7e64
laxmi <laxminarayanap2131@gmail.com>
2026-05-04 03:00:05 +0000

    pushed to github

--- deploy.sh
+++ deploy.sh
@@ -4,12 +4,16 @@ sudo dnf install httpd -y
 sudo systemctl start httpd
 sudo systemctl enable httpd
 
-echo "<html>
-<head><title>My App</title></head>
+sudo bash -c 'cat > /var/www/html/index.html <<EOF
+<html>
+<head>
+<title>My App</title>
+</head>
 <body>
 <h1>Welcome to My Simple Web App 🚀</h1>
 <p>Deployed correctly using Shell Script</p>
 </body>
-</html>" | sudo tee /var/www/html/index.html
+</html>
+EOF'
 
 sudo systemctl restart httpd
