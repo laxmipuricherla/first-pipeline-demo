e1ebb7d9f5da187430ed7148e9a4583eb1df4bc9
laxmi <laxminarayanap2131@gmail.com>
2026-05-04 02:51:11 +0000

    updated the deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,24 +1,15 @@
 #!/bin/bash
 
-# Install Apache
 sudo dnf install httpd -y
-
-# Start Apache
 sudo systemctl start httpd
 sudo systemctl enable httpd
 
-# Create clean HTML file (ONLY HTML, no commands)
-cat <<EOF | sudo tee /var/www/html/index.html
-<html>
-<head>
-<title>My Web App</title>
-</head>
+echo "<html>
+<head><title>My App</title></head>
 <body>
 <h1>Welcome to My Simple Web App 🚀</h1>
 <p>Deployed correctly using Shell Script</p>
 </body>
-</html>
-EOF
+</html>" | sudo tee /var/www/html/index.html
 
-# Restart Apache
 sudo systemctl restart httpd
